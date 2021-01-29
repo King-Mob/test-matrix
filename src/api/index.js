40b9464c303f523c3c30fee6541da78b8f83aeef
John Evans <38507954+King-Mob@users.noreply.github.com>
2021-01-29 16:59:54 +0000

    added room history and attempting message send

--- src/api/index.js
+++ src/api/index.js
@@ -11,7 +11,8 @@ export const syncWithMatrix = async () => {
     const client = sdk.createClient({
         baseUrl: UserInfo.baseUrl,
         accessToken: UserInfo.accessToken,
-        userId: UserInfo.userId
+        userId: UserInfo.userId,
+        deviceId: "my-device-id"
     });
 
     console.log(client.getAccessToken());
@@ -28,7 +29,34 @@ export const syncWithMatrix = async () => {
 const tryRooms = async (client) => {
     let rooms = await client.getRooms();
     rooms.forEach(room => {
-    console.log(room.roomId);
+        console.log(room.roomId);
+
+        let members = room.getJoinedMembers();
+        members.forEach(member => {
+            console.log(member.name);
+        });
+
+        room.timeline.forEach(t => {
+            console.log(JSON.stringify(t.event.content));
+        });
+
+        trySendMessage(room,client);
     });
 }
 
+const trySendMessage = (room, client) => {
+    let testRoomId = room.roomId;
+
+    let content = {
+        "body": "Hello from my client",
+        "msgtype": "m.text"
+    };
+
+    client.sendEvent(testRoomId, "m.room.message", content, "")
+        .then((res) => {
+            console.log(res)
+        // message sent successfully
+        }).catch((err) => {
+            console.log(err);
+    });
+}
